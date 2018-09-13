98106b45e31375c25d25d47e03d4a4fcf254ce8b
kharuk <frends.nastya@gmail.com>
2018-09-14 00:54:53 +0300

    all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let countOfZeros = 0;
+    let counter = 5;
+    while(counter <= number){
+        countOfZeros += Math.trunc(number / counter);
+        counter *= 5;
+    }
+    return countOfZeros;
 }
